853cc761da600883d279553e03aea3c13ea17280
Sevinch571 <TurayevaSevinch571@gmail.com>
2023-12-10 20:43:24 +0500

    header, nav and js function added

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const searchIcon=document.querySelector(".searchIcon");
+// const searchIcon=document.querySelector(".searchIcon");
 
-searchIcon.addEventListener("click",()=>{
-})
-document.addEventListener("keydown",(e)=>{
-    console.log(e)
-})
+// searchIcon.addEventListener("click",()=>{
+// })
+// document.addEventListener("keydown",(e)=>{
+//     console.log(e)
+// })
+
+const modal = document.querySelector('#modal');
+const btn = document.querySelector('#openModal');
+const close = document.querySelector('.close');
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+close.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
+
+
