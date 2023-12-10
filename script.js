88dce95ea5ac280bf899ee8bfb8964f79cab1450
bobur2006coder <boburdilmurodov702@gmail.com>
2023-12-10 23:01:19 +0500

    almost top food section finished

--- script.js
+++ script.js
@@ -27,3 +27,56 @@ window.onclick = function (event) {
 };
 
 
+const dataBase=[
+  {
+    imgUrl:"./img/images/pitsa.png",
+    cardTitle:"Blaze Pizza",
+    clock:"11.00 AM - 10.00 PM",
+  },
+  {
+    imgUrl:"./img/images/pitsa2.png",
+    cardTitle:"Pizza Ranch",
+    clock:"09.00 AM - 10.00 PM",
+  },
+  {
+    imgUrl:"./img/images/pitsa3.png",
+    cardTitle:"	Dion’s Pizza Hut",
+    clock:"10.00 AM - 12.00 PM",
+  },
+  {
+    imgUrl:"./img/images/pitsa4.png",
+    cardTitle:"Royel Burger",
+    clock:"11.00 AM - 10.00 PM",
+  },
+  {
+    imgUrl:"./img/images/pitsa5.png",
+    cardTitle:"KFC Restaurant",
+    clock:"09.00 AM - 10.00 PM",
+  },
+  {
+    imgUrl:"./img/images/pitsa6.png",
+    cardTitle:"Star Food",
+    clock:"10.00 AM - 12.00 PM",
+  },
+]
+
+const Restaurant=document.getElementById("Restaurant");
+
+for(let i =0;  i <=dataBase.length; i++){
+   Restaurant.innerHTML+=`
+   <div class="col-12 col-sm-3 bg-white border-left-0" style="border-radius: 50px 0px; border: 1px solid transparent;">
+   <div class="">
+       <img src="${dataBase[i].imgUrl}" alt="" class="cardImg">
+   </div>
+   <div class="p-1 pt-5 pb-5 ">
+       <h4 style="color:#363853;">${dataBase[i].cardTitle}</h4>
+       <div class="d-flex align-items-center justify-content-between">
+           <i class="fas fa-stopwatch fs-4"> <span class="fs-5 fw-light">${dataBase[i].clock}</span></i>
+           <button class="topFood__btn">
+               <i class="fas fa-arrow-right text-white"></i>
+           </button>
+       </div>
+   </div>
+</div>
+   `
+}
